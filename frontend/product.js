8975bafdbe69ff027b9b7f1ab7b4d969018e29ca
Mancy <abood1only@hotmail.com>
2025-03-14 16:56:09 -0400

    Added the final part of the project

--- frontend/product.js
+++ frontend/product.js
@@ -1,25 +1,45 @@
-const varietiesList = [
-  {
-    name: "EASY OFF Heavy Duty",
-    price: 7.99,
-  },
-  {
-    name: "EASY OFF Light Duty",
-    price: 4.99,
-  },
-];
-function addToCart(dropdown) {
+function addToCart() {
   let dropdownList = document.getElementById("dropdown");
+  let myCurrentItem = dropdownList.options[dropdownList.selectedIndex].text;
   let myCurrentPrice = Number(dropdownList.value);
   let myStoredPrice = Number(localStorage.getItem("totalPrice"));
   let myTotalPrice = myCurrentPrice + myStoredPrice;
-  alert("Total price is $" + myTotalPrice.toFixed(2));
+  alert(
+    myCurrentItem +
+      " added to your Cart. \nTotal price is $" +
+      myTotalPrice.toFixed(2)
+  );
   localStorage.setItem("totalPrice", myTotalPrice.toFixed(2));
 }
-function UpdatePrice(price) {
+function jsDropdown() {
+  let dropdownList = document.getElementById("dropdown");
+  const listOfItems = [
+    {
+      name: "EASY OFF Heavy Duty 12 oz",
+      price: 7.99,
+    },
+    {
+      name: "EASY OFF Light Duty 12 oz",
+      price: 4.99,
+    },
+  ];
+  listOfItems.forEach((item) => {
+    let option = document.createElement("option");
+    option.text = item.name;
+    option.value = item.price;
+    dropdownList.add(option);
+  });
+}
+function updatePrice(price) {
   document.getElementById("current-price").innerHTML = "$" + price;
 }
-function dropdown(varietiesList) {
-  let dropdownList = document.getElementsById(varietiesList);
-  console.log(varietiesList);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  jsDropdown();
+
+  let dropdownList = document.getElementById("dropdown");
+  dropdownList.addEventListener("change", function () {
+    let itemPrice = dropdownList.value;
+    updatePrice(itemPrice);
+  });
+});
+//jsDropdown();
